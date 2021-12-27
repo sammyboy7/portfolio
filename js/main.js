78b39cf1d5b9d7f45dc4067523926b9a3a74a652
Samuel Martins <samuel.martins4.sm@gmail.com>
2021-12-27 15:40:57 +0300

    css changes mostly to the home page

--- js/main.js
+++ js/main.js
@@ -185,3 +185,48 @@ if (
 
     share();
 }
+
+let span = document.querySelector(".slide-toggle");
+let project_slide = document.querySelector(".project-slide");
+let product_page = Math.ceil(project_slide.length / 4);
+let l = 0;
+let movePer = 25.34;
+let maxMove = 203;
+
+// mobile_view
+let mob_view = window.matchMedia("(max-width: 768px)");
+if (mob_view.matches) {
+    movePer = 50.36;
+    maxMove = 504;
+}
+
+let right_mover = () => {
+    l = l + movePer;
+    if (project_slide == 1) {
+        l = 0;
+    }
+    for (const i of project_slide) {
+        if (l > maxMove) {
+            l = l - movePer;
+        }
+        i.style.left = "-" + l + "%";
+    }
+};
+let left_mover = () => {
+    l = l - movePer;
+    if (l <= 0) {
+        l = 0;
+    }
+    for (const i of project_slide) {
+        if (product_page > 1) {
+            i.style.left = "-" + l + "%";
+        }
+    }
+};
+
+span[1].onclick = () => {
+    right_mover();
+};
+span[0].onclick = () => {
+    left_mover();
+};
